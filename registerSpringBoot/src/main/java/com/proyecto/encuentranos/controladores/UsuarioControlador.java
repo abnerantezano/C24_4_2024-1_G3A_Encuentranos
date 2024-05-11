6ede1ebdf9daea3ebab4820570dab4aa921270d4
JosueAmbrosio <josue.ambrosio@tecsup.edu.pe>
2024-05-11 11:44:37 -0500

    Cambios Oauth2

--- registerSpringBoot/src/main/java/com/proyecto/encuentranos/controladores/UsuarioControlador.java
+++ registerSpringBoot/src/main/java/com/proyecto/encuentranos/controladores/UsuarioControlador.java
@@ -3,6 +3,12 @@ package com.proyecto.encuentranos.controladores;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.web.bind.annotation.*;
 
 import com.proyecto.encuentranos.modelos.UsuarioModelo;
@@ -18,14 +24,40 @@ public class UsuarioControlador {
     @Autowired
     UsuarioServicio usuarioServicio;
     
+    @Autowired
+    private OAuth2AuthorizedClientService authorizedClientService;
+
+    @Autowired
+    private ClientRegistrationRepository clientRegistrationRepository;
     
     @GetMapping("/listar")
-    public ArrayList<UsuarioModelo> obtenerUsurios(){
-    	return (ArrayList<UsuarioModelo>)usuarioServicio.obtenerUsuarios();
+    public ArrayList<UsuarioModelo> obtenerUsuarios() {
+    	return (ArrayList<UsuarioModelo>) usuarioServicio.obtenerUsuarios();
     }
     
 	@PostMapping("/agregar")
 	public UsuarioModelo guardarUsuario(@RequestBody UsuarioModelo usuario) {
 		return this.usuarioServicio.guardarUsuario(usuario);
 	}
+
+	@GetMapping("/token")
+	public String obtenerToken() {
+	    // Obtener la autenticación actual
+	    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+	    
+	    // Verificar si la autenticación es de tipo OAuth2
+	    if (authentication instanceof OAuth2AuthenticationToken) {
+	        // Obtener el token de acceso usando OAuth2AuthorizedClientService
+	        OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
+	        OAuth2AccessToken accessToken = authorizedClientService.loadAuthorizedClient(
+	                oauthToken.getAuthorizedClientRegistrationId(),
+	                oauthToken.getName()
+	        ).getAccessToken();
+	        // Devolver el token de acceso como String
+	        return accessToken.getTokenValue();
+	    }
+	    // Devolver null si la autenticación no es de tipo OAuth2
+	    return null;
+	}
+
 }
