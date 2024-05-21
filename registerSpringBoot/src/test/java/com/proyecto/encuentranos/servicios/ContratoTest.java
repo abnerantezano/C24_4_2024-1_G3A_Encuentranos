efa956a3b76d43346ef2a4b5e7ff082869a2c1dc
JosueAmbrosio <josue.ambrosio@tecsup.edu.pe>
2024-05-21 11:46:15 -0500

    Subiendo test

---
+++ registerSpringBoot/src/test/java/com/proyecto/encuentranos/servicios/ContratoTest.java
@@ -0,0 +1,19 @@
+package com.proyecto.encuentranos.servicios;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import com.proyecto.encuentranos.modelos.ContratoModelo;
+import com.proyecto.encuentranos.modelos.ContratoPk;
+import com.proyecto.encuentranos.modelos.ProveedorModelo;
+
+class ContratoTest {
+
+	@Test
+	void test() {
+
+	}
+
+}
